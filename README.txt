d013cec1735b011d95f7e9a6e8776c8b7fcab0dd
mialondon <github@miaridge.com>
2010-11-06 16:51:02 +0000

    first commit

---
+++ README.txt
@@ -0,0 +1,18 @@
+README.txt
+
+Bits of code used to access various museum APIs.  
+
+I'm working in PHP/MySQL at the moment, and generally importing data to save APIs hits so that's what most of the scripts do to start with.
+
+Currently working:
+
+Science Museum 'Cosmic Collections' API
+http://www.sciencemuseum.org.uk/objectapi/cosmosculturepublic.svc/MuseumObjects
+
+In progress:
+Powerhouse Museum API
+http://api.powerhousemuseum.com/
+
+If you see ### that's a bit I have to go back and tidy - it's all a bit messy and very alpha.
+
+Tweet @mia_out, http://miaridge.com or http://openobjects.org.uk for more info, contact, etc.
---
+++ ScienceMuseum/import_data.php
@@ -0,0 +1,63 @@
+<?php
+
+echo "Loading file... ";
+
+// loading a local file
+$file = 'data/ScienceMuseumCosmicCollectionsObjects.xml';
+
+if (file_exists($file)) {
+    $data = simplexml_load_file($file);
+ 
+    //print_r($data);
+} else {
+    exit('Failed to open $file.');
+}
+
+// set up database connections
+// global $wpdb; // run prepare just in case there's dodgy data in the XML ###
+$db_account = 'root';
+$db_password = 'root';
+$db_host = 'localhost';
+$db_name = 'wordpress_mu';
+$db_objecttable = 'wp_matchy_objects';
+$db_objecttablefields = 'name, accession_number, institution, data_source_url,
+      interpretative_date, interpretative_place, image_url';
+$sql = '';
+
+// open the XML file and start to process it
+// getting to specific bits
+
+if( sizeof($data) > 0 ){
+  
+  // create db connection cos there's something to store
+  $dbc = @mysql_connect ($db_host, $db_account, $db_password) 
+    or die ('Could not connect to MySQL:'.mysql_error());
+  @mysql_select_db($db_name)
+     or die ('Could not select the database:'.mysql_error());
+  
+  echo "Building SQL string... ";
+
+	foreach ($data->MuseumObjectsResult->MuseumObject as $museumobject) {
+	  $object_name = filter_var($museumobject->Name,FILTER_SANITIZE_ENCODED);
+    $accession_number = filter_var($museumobject->AccessionNumber,FILTER_SANITIZE_ENCODED);
+    $institution = 'Science Museum';
+    $data_source_url = 'http://'.filter_var($museumobject->PermanentURI,FILTER_SANITIZE_ENCODED);
+    $interpretative_date = filter_var($museumobject->InterpretativeDate,FILTER_SANITIZE_ENCODED);
+    $interpretative_place = filter_var($museumobject->InterpretativePlace,FILTER_SANITIZE_ENCODED);
+    $image_url = 'http://www.sciencemuseum.org.uk/hommedia.ashx?id='.$museumobject->Image->Id.'&size=Small';
+    
+    $sql = "insert into $db_objecttable  ($db_objecttablefields) " .
+    "values ('$object_name','$accession_number','$institution', '$data_source_url',
+    '$interpretative_date', '$interpretative_place', '$image_url') ";
+    
+    mysql_query($sql) or die ('Something bad happened:'.mysql_error());
+        
+    // echo $sql; 
+
+	}
+
+mysql_close($dbc); // tidy up more? ###
+
+}
+
+?>
